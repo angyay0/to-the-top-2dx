679e3fc3edaf5fe52b30504123648fa3a8690679
angyay0 <angel.ed.pec@gmail.com>
2018-10-22 16:29:34 -0500

    Level Generator

--- Classes/Core/Utils/Toolkit.cpp
+++ Classes/Core/Utils/Toolkit.cpp
@@ -16,17 +16,99 @@ Toolkit *Toolkit::getInstance(){
     return instance;
 }
 
-int** Toolkit::getLevelMap(float level,GAME_DIFFICULTY dif,struct PlayerHability hab) {
+int Toolkit::getAvailableSpaces(int height, int maxHeight, int level, GAME_DIFFICULTY difficulty) {
+    int spaces = 2 - ((int) (1 * (level%10+(level/difficulty)))); //1~10, 1, 2, 3 y 5
+    spaces -= (height<(maxHeight-1))?2:0;
+    spaces = (spaces<0)?1:spaces;
+
+    return height<maxHeight-1?spaces:0;
+}
+
+int Toolkit::getElementPerSpot(int scape_spaces, bool canBeEnded) {
+    int length = 1; //this depends on which kind o blocks are availables
+    int count = 0;
+    length += canBeEnded?1:0;
+    length += (scape_spaces>0)?scape_spaces:0;
+
+    int *availableItems = new int[length+1];
+    availableItems[count] = BLOCK_ITEM_MASK; count++;
+    if(canBeEnded){availableItems[count] = STGCL_ITEM_MASK;count++;}
+    for(int i=0;i<scape_spaces;i++){availableItems[count]=SPACE_ITEM_MASK;count++;}
+    availableItems[count] = BLOCK_ITEM_MASK;
+
+    int item = arc4random() % (length+1);
+    return availableItems[item];
+}
+
+int** Toolkit::getLevelMap(int level,GAME_DIFFICULTY dif,struct PlayerHability hab) {
     int** levelMap = 0;
+    int scapeSpots = 0;
+    bool endSpot = false;
+
+    //Calculate Height for the level
     int height = LEVEL_BASE_HEIGHT;
+
+    //Instantiate basic objects
     levelMap = new int*[height];
-    //TODO
-    for(int c=0;c<height;c++) {
-        levelMap[c] = new int[LEVEL_BASE_WIDTH];
-        for(int w=0;w<LEVEL_BASE_WIDTH;w++) {
-            levelMap[c][w] = BLOCK_ITEM_MASK;
+
+    //Level Core Generation
+    for(int h=0;h<height;h++) {
+        levelMap[h] = new int[LEVEL_BASE_WIDTH];
+        scapeSpots = this->getAvailableSpaces(h,height,level,dif);
+        endSpot = (h==height-1);
+
+        for(int w=0;w<LEVEL_BASE_WIDTH;w++) {
+            int item = this->getElementPerSpot(scapeSpots,endSpot);
+            if (item==SPACE_ITEM_MASK) scapeSpots = (scapeSpots<1)?scapeSpots:scapeSpots--;
+            if (endSpot) endSpot = !(item==STGCL_ITEM_MASK);
+
+            levelMap[h][w] = item;
         }
     }
+
+    this->threatNoSpacePath(levelMap,height,LEVEL_BASE_WIDTH);
+    this->threatNoWinPath(levelMap,height,LEVEL_BASE_WIDTH);
     
     return levelMap;
+}
+
+void Toolkit::threatNoSpacePath(int **levelMap, int height, int width) {
+    bool hasSpaceSpots = false;
+    int space1,space2;
+
+    for(int i=0;i<height-1;i++) {
+        hasSpaceSpots = false;
+
+        for(int j=0;j<width;j++){
+            if (levelMap[i][j]==SPACE_ITEM_MASK){ //At least one
+                hasSpaceSpots = true;
+                break;
+            }
+        }
+
+        if (!hasSpaceSpots) {
+            do {
+                space1 = arc4random() % width;
+                space2 = arc4random() % width;
+            }while(space2 == space1);
+
+            levelMap[i][space1] = SPACE_ITEM_MASK;
+            levelMap[i][space2] = SPACE_ITEM_MASK;
+        }
+    }
+}
+
+void Toolkit::threatNoWinPath(int **levelMap, int height, int width) {
+    bool hasWinSpace = false;
+    for(int i=0;i<width;i++) {
+        if (levelMap[height-1][i] == STGCL_ITEM_MASK) {
+            hasWinSpace = true;
+            break;
+        }
+    }
+
+    if (!hasWinSpace) {
+        int item = arc4random() % width;
+        levelMap[height-1][item] = STGCL_ITEM_MASK;
+    }
 }
--- Classes/Core/Utils/Toolkit.h
+++ Classes/Core/Utils/Toolkit.h
@@ -17,8 +17,14 @@ private:
     Toolkit(Toolkit const&){};
     Toolkit& operator = (Toolkit const&){};
     static Toolkit *instance;
+
+    //Methods for level generator
+    int getAvailableSpaces(int height,int maxHeight,int level,GAME_DIFFICULTY difficulty);
+    int getElementPerSpot(int scape_spaces,bool canBeEnded);
+    void threatNoSpacePath(int **levelMap,int height,int width);
+    void threatNoWinPath(int **levelMap,int height,int width);
 public:
     static Toolkit *getInstance();
-    int** getLevelMap(float level,GAME_DIFFICULTY dif,struct PlayerHability hab);
+    int** getLevelMap(int level,GAME_DIFFICULTY dif,struct PlayerHability hab);
 };
 #endif /* Toolkit_h */
